2fb28b38619969c6f074b8538b2293462fd195fd
Jinuk <jitoor610@gmail.com>
2022-04-30 23:21:28 +0900

    [Chore] #5 - AppBar 적용

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:ttangkkeusmarket/widgets/AppBar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Home Screen",
-        style: TextStyle(fontSize: 30),
-      ),
-    );
+    return Scaffold(
+        appBar: BaseAppBar(appBar: AppBar(), title: "땅끗마켓", center: true),
+        body: Center(
+            child: Text(
+          "Home Screen",
+          style: TextStyle(fontSize: 30),
+        )));
   }
 }
