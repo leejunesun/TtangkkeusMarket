d13986db467007cda98557d5a4d94e2e63eba017
최진욱 <74299463+JJinuk@users.noreply.github.com>
2022-05-22 23:34:14 +0900

    Revert "[Chor] #21 - MyPage 로그인 구현"

--- lib/src/screens/mypage_screen.dart
+++ lib/src/screens/mypage_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import './login_screen.dart';
-import 'package:ttangkkeusmarket/src/widgets/base_appbar.dart';
+import 'package:ttangkkeusmarket/src/widgets/appbar.dart';
 
 class MypageScreen extends StatelessWidget {
   const MypageScreen({Key? key}) : super(key: key);
@@ -8,53 +7,12 @@ class MypageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: BaseAppBar(appBar: AppBar(), title: "마이페이지", center: true),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                  width: 340,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(_loginRoute());
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFF6C544),
-                      elevation: 0.0,
-                      shadowColor: Colors.transparent,
-                    ),
-                    child: Text(
-                      "로그인/회원가입",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'NotoSans',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ))
-            ],
-          ),
-        ));
+      appBar: BaseAppBar(appBar: AppBar(), title: "마이페이지", center: true),
+      body: Center(
+          child: Text(
+        "Mypage Screen",
+        style: TextStyle(fontSize: 30),
+      )),
+    );
   }
-}
-
-Route _loginRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => LoginScreen(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      const begin = Offset(0.0, 1.0);
-      const end = Offset.zero;
-      const curve = Curves.ease;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
 }
