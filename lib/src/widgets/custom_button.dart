4bbf5d987227574f6dbf3f24f93a444b53f923a7
Jinuk <jitoor610@gmail.com>
2022-05-30 13:22:36 +0900

    Auto stash before merge of "develop" and "origin/develop"

--- lib/src/widgets/custom_button.dart
+++ lib/src/widgets/custom_button.dart
@@ -60,3 +60,55 @@ class CustomButton extends StatelessWidget {
     );
   }
 }
+// class CustomButton {
+//   static Widget roundedIconButton(
+//       {required Function onTap,
+//       required String title,
+//       Color? backgroundColor,
+//       double? width,
+//       double? height,
+//       double? radius,
+//       Icon? icon,
+//       TextStyle? titleTextstyle}) {
+//     return GestureDetector(
+//       onTap: () => onTap(),
+//       child: Container(
+//         width: width ?? 345,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             if (icon != null) icon,
+//             if (icon != null) SizedBox(width: 20),
+//             Text(title,
+//                 style: titleTextstyle ??
+//                     TextStyle(fontSize: 16, color: Colors.black))
+//           ],
+//         ),
+//         height: height ?? 50,
+//         decoration: BoxDecoration(
+//             color: backgroundColor ?? Colors.blue,
+//             borderRadius: BorderRadius.circular(radius ?? 5)),
+//       ),
+//     );
+//   }
+
+//   static Widget iconButton({
+//     required Function onTap,
+//       required Icon icon,
+//       Color? backgroundColor,
+//       double? width,
+//       double? height,
+//       double? radius,}) {
+//     return GestureDetector(
+//       onTap: () => onTap(),
+//       child: Container(
+//         width: width ?? 50,
+//         child: icon,
+//         height: height ?? 50,
+//         decoration: BoxDecoration(
+//             color: backgroundColor ?? Colors.blue,
+//             borderRadius: BorderRadius.circular(radius ?? 8)),
+//       ),
+//     );
+//   }
+// }
